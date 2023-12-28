bcaf053a2c421a36dd529e192854e180ad3e0033
Tunahan Turgut <114873393+Bizbize60@users.noreply.github.com>
2023-12-28 19:31:56 +0300

    Add files via upload

---
+++ yemek.py
@@ -0,0 +1,64 @@
+class Person():
+    def __init__(self,isim):
+        self.isim=isim
+        
+    def hesap(self,yemek):
+        if yemek=="et":
+            fiyat=50
+           
+
+
+def fonksiyon(Object):
+    output=1
+    if output=="ABCDEFG":
+        Object.hesap("et")
+    elif output==("CDGHSFDDS"):
+        print(1)
+        
+    
+  
+    
+    
+    
+    
+    
+    
+    
+    
+        
+        
+        
+    
+    
+    
+    
+    
+    
+    
+kişi1=Person("ahmet")
+
+    
+    
+   
+   
+    
+   
+            
+        
+
+
+
+
+  
+
+        
+  
+    
+    
+    
+    
+    
+    
+    
+
+
